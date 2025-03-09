9b8c8bebaae77d208b799f699f2528e89b18ba19
Chael <sparkythedog121111@gmail.com>
2025-03-09 13:48:52 -0400

    4.2: added event listener to button.

--- Assignment4/Part2/main.js
+++ Assignment4/Part2/main.js
@@ -38,7 +38,7 @@ for (let i = 0; i < imageFilenames.length; i++) {
     newImage.setAttribute('src', `images/${imageFilenames[i]}`);
     newImage.setAttribute('alt', `${imageAltText[i]}`);
 
-    newImage.addEventListener("click", (e) => {
+    newImage.addEventListener("click", () => {
         displayedImage.setAttribute('src', `images/${imageFilenames[i]}`);
         displayedImage.setAttribute('alt', `${imageAltText[i]}`);
     });
@@ -47,3 +47,16 @@ for (let i = 0; i < imageFilenames.length; i++) {
 }
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener("click", (event) => {
+    buttonClass = btn.getAttribute("class");
+    if(buttonClass === "dark") {
+        btn.setAttribute("class", "light");
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 50%)";
+    } else {
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
+    }
+});
